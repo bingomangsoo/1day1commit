43881a3f7ceea7a0bc59e1d85fc8f1fde737de8c
bingomangsoo <101463273+bingomangsoo@users.noreply.github.com>
2023-02-01 16:44:13 +0900

    [Bronze I] Title: 이항 계수 1, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -6,15 +6,20 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        System.out.println(C(n)/(C(n-k)*C(k)));
+        int [][] arr = new int[n+1][n+1];
 
-
-    }
-    public static int C(int x){
-        int result = 1;
-        for (int i =1; i<=x; i++){
-            result *= i;
+        for (int i=0; i<n+1; i++){
+            arr[i][i] = 1;
+            arr[i][0] = 1;
         }
-        return result;
+
+        for (int i=1; i<n+1; i++){
+            for (int j=1; j<k+1; j++){
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+        System.out.println(arr[n][k]);
+
+
     }
 }
