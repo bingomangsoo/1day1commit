3af17c198e440db6fa5345730386f15e1db859b8
bingomangsoo <101463273+bingomangsoo@users.noreply.github.com>
2023-02-03 16:10:02 +0900

    [Bronze I] Title: 더하기 사이클, Time: 236 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int n1 = n/10;
+        int n2 = n%10;
+
+
+        int sum = 0;
+        int result =0;
+        int cnt = 0;
+        while(true){
+            sum = n1+n2;
+            n1 = n2;
+            n2 = sum%10;
+            result = n1*10+n2;
+            cnt++;
+            if (result == n){
+                break;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
