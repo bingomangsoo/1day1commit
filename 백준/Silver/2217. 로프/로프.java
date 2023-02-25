d901743c3722c9647dbf43d8a1be3e67c57a6154
bingomangsoo <101463273+bingomangsoo@users.noreply.github.com>
2023-02-25 21:00:59 +0900

    [Silver IV] Title: 로프, Time: 956 ms, Memory: 109360 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,28 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int [] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int max = 0;
+        for (int i =0; i<n; i++){
+            if (arr[i]*(n-i) > max){
+                max = arr[i]*(n-i);
+            }
+        }
+        System.out.println(max);
+    }
+
+}
