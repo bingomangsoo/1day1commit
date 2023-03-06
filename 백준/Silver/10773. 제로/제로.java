cae2954fec360a5fe6f81b4202b0e1c07d5c851d
bingomangsoo <101463273+bingomangsoo@users.noreply.github.com>
2023-03-06 12:09:34 +0900

    [Silver IV] Title: 제로, Time: 860 ms, Memory: 109864 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,31 @@
+
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i =0; i<n; i++){
+            int num = sc.nextInt();
+
+            if (num != 0){
+                stack.push(num);
+            } else {
+                stack.pop();
+            }
+        }
+
+        int sum = 0;
+        Iterator iterator = stack.iterator();
+
+        while (iterator.hasNext()){
+            sum += (int)iterator.next();
+        }
+        System.out.println(sum);
+    }
+}
